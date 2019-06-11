0c080763b94032fae393227cfd78d8c782f58f05
HaVanHung290598 <32954017+HaVanHung290598@users.noreply.github.com>
2019-06-11 08:47:14 +0700

    cart
    Display product information in the cart icon on the menu

--- src/main/java/com/hvh/controller/HomeController.java
+++ src/main/java/com/hvh/controller/HomeController.java
@@ -1,22 +1,30 @@
 package com.hvh.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.hvh.model.CartDTO;
 import com.hvh.model.ProductAttributeDTO;
 import com.hvh.model.ProductDTO;
+import com.hvh.model.UserDTO;
 import com.hvh.service.CartService;
 import com.hvh.service.ProductAttributeService;
 import com.hvh.service.ProductService;
+import com.hvh.service.UserService;
 
 @Controller
 public class HomeController {
@@ -30,12 +38,40 @@ public class HomeController {
 	@Autowired
 	CartService cartService;
 	
+	@Autowired
+	UserService userService;
+	
+	public List<Map<String, Object>> getListCart() {
+		List<Map<String, Object>> params = new ArrayList<Map<String, Object>>();
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		UserDTO userDTO = userService.getUserByUsername(username);
+		
+		List<CartDTO> carts = cartService.getListCartByUser(userDTO.getId());
+		for(CartDTO cart : carts) {
+			ProductDTO product = productService.getProductById(cart.getProduct_id());
+			Map<String, Object> param = new HashMap<String, Object>();
+			param.put("product-id", cart.getProduct_id());
+			param.put("user-id", cart.getUser_id());
+			param.put("color", cart.getColor());
+			param.put("size", cart.getSize());
+			param.put("quantity", cart.getQuantity());
+			param.put("product-name", product.getProduct_name());
+			param.put("image", product.getImage());
+			param.put("price", product.getUnit_price());
+			params.add(param);
+		}
+		return params;
+	}
+	
 	@RequestMapping(value= {"/","/home"}, method = RequestMethod.GET)
 	public String home(Model model) {
 		List<ProductDTO> productDTOs = productService.getListProduct(6,4);
 		model.addAttribute("productDTOs", productDTOs);
 		model.addAttribute("display", "none");
-		model.addAttribute("color", "#f1b8c4");
+		model.addAttribute("color", "#f1b8c4");		
+		model.addAttribute("params", getListCart());		
 		return "home";
 	}
 	@RequestMapping(value="/collection", method = RequestMethod.GET)
@@ -51,6 +87,7 @@ public class HomeController {
 		model.addAttribute("page", "PRODUCT");
 		model.addAttribute("display", "block");
 		model.addAttribute("color", "#f1b8c4");
+		model.addAttribute("params", getListCart());
 		return "collection";
 	}
 	@RequestMapping(value="/productSingle", method = RequestMethod.GET)
@@ -68,6 +105,7 @@ public class HomeController {
 		model.addAttribute("home", "HOME");
 		model.addAttribute("page", "PRODUCT SINGLE");
 		model.addAttribute("color", "#f1b8c4");
+		model.addAttribute("params", getListCart());
 		return "productSingle";
 	}
 }
--- src/main/webapp/WEB-INF/views/collection/header-collection.jsp
+++ src/main/webapp/WEB-INF/views/collection/header-collection.jsp
@@ -26,35 +26,31 @@
 						</security:authorize>
 						<c:choose>
 							<c:when test="${not empty user.username}">
-								<li><a href=""><i class="fas fa-shopping-cart"></i>[0]</a>
+								<li><a href=""><i class="fas fa-shopping-cart"></i>[${params.size()}]</a>
 									<ul class="submenu2">
 										<li>New products</li>
-										<li>
-											<div class="row">
-												<div class="col-md-2"><img src="<c:url value="/resources/images/product-4.jpg"/>" width="40px" height="auto"/></div>
-												<div class="col-md-6 list-products">
-													<p>Young Woman Wearing Dress</p>
-													<p>SIZE: SMALL</p>
+										<c:forEach items="${params}" var="s">
+											<li>
+												<div class="row">
+													<div class="col-md-2"><img src="<c:url value="/resources/images/${s.get('image')}.jpg"/>" width="40px" height="auto"/></div>
+													<div class="col-md-10">
+														<div class="row">
+															<div class="col-md-8 list-products">
+																<p>${s.get("product-name")}</p>
+															</div>
+															<div class="col-md-4">
+																<p>$${s.get("price")}</p>
+															</div>
+														</div>
+														<div class="row">
+															<div class="col-md-12 proc-classification">
+																<p>Product classification: ${s.get("size")},${s.get("color")}</p>
+															</div>
+														</div>
+													</div>
 												</div>
-												<div class="col-md-4">
-													<p>$120.00</p>
-													<a href="" class="delete-product">Delete</a>
-												</div>
-											</div>
-										</li>
-										<li>
-											<div class="row">
-												<div class="col-md-2"><img src="<c:url value="/resources/images/product-5.jpg"/>" width="40px" height="auto"/></div>
-												<div class="col-md-6 list-products">
-													<p>Young Woman Wearing Dress</p>
-													<p>SIZE: SMALL</p>
-												</div>
-												<div class="col-md-4">
-													<p>$120.00</p>
-													<a href="" class="delete-product">Delete</a>
-												</div>
-											</div>
-										</li>
+											</li>
+										</c:forEach>
 										<li class="carts"><a href="cart">VIEW CART</a></li>
 									</ul>
 								</li>
